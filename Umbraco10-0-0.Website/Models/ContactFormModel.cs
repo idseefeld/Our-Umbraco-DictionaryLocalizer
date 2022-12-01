7084ea51563e944b590585392d78e7948bcdfbd9
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2022-12-01 21:25:34 +0100

    add v10.0.0

---
+++ Umbraco10-0-0.Website/Models/ContactFormModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Umbraco10_0_0.Website.Models
+{
+    public class ContactFormModel : Our.Umbraco.DictionaryLocalizer.Models.DictionaryDataAnnotationBaseModel
+    {
+        [Required(ErrorMessage = "#RequiredErrorMessage.name")]
+        [Display(Name = "#nameLabel")]
+        public string? FirstAndLastName { get; set; }
+
+        [Required(ErrorMessage = "#RequiredErrorMessage.email")]
+        [EmailAddress(ErrorMessage = "#InvalidErrorMessage.email")]
+        [Display(Name = "#emailLabel")]
+        public string? Email { get; set; }
+
+        [Display(Name = "#subjectLabel")]
+        [Required(ErrorMessage = "#RequiredErrorMessage.subject")]
+        public string? Subject { get; set; }
+
+        [Display(Name = "#messageLabel")]
+        [Required(ErrorMessage = "#RequiredErrorMessage.message")]
+        public string? Message { get; set; }
+    }
+}
---
+++ Umbraco10-0-0.Website/Models/ContactMailOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Umbraco10_0_0.Website.Models
+{
+    public class ContactMailOptions
+    {
+        public const string ContactMail = "ContactMail";
+
+        public string? To { get; set; }
+        public string? ToName { get; set; }
+    }
+}
